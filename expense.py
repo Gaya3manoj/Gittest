828c8d47e926ad6ece7b131a922ec23f3576771d
Gaya3manoj <gaya3manoj@outlook.com>
2021-09-17 11:59:22 +0530

    test

--- expense.py
+++ expense.py
@@ -6,16 +6,26 @@ Created on Thu Sep 16 12:08:37 2021
 """
 
 import pandas as pd
+import numpy as np
+import sys
 
 
 
 
 df = pd.read_excel('C:\Expenses\Sept\Expenses_Sheet.xlsx')
-df['Total_Amt_Spent'] = df.loc[: , ['Gayathri', 'Manoj']].sum(axis=1)
+df['Total_Amt_Spent'] = df.loc[: , ['Gayathri', 'Manoj', 'Appa']].sum(axis=1)
 print(df)
 df1=df.agg({'Total_Amt_Spent': 'sum'})
 print(df1)
-df1.to_excel('C:\Expenses\Sept\Totalamtspent.xlsx',sheet_name='Output')
+df1.to_excel('C:\Expenses\Sept\Expenses_Sheet.xlsx',sheet_name='Output')
+sys.stdout.flush()
+
+#grp = df.groupby('Expenses_description')
+#df3=grp.aggregate(np.sum)
+#for Expenses_description, group in grp:
+    #print(Expenses_description)
+#    print(group)
+#pd.DataFrame(grp).to_excel('C:\Expenses\Sept\Totalamtspent1.xlsx',sheet_name='Output')
 
 
 
